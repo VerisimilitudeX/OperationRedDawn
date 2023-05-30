0eea4f492b552eea38218e9f0f8273acf3ef7f91
IcebergSnow <116831379+IcebergSnow@users.noreply.github.com>
2023-05-30 21:10:12 +0000

    committing

--- Message Encoder/Puzzles.java
+++ Message Encoder/Puzzles.java
@@ -6,7 +6,7 @@ public class Puzzles {
             case "1234" -> "hello";
             case "1105" -> "george kennan";
             case "2966" -> "rishabh is a bozo";
-            default -> "Incorrect code entered.";
+            default -> "No noticable response was recieved.";
         };
         return clue;
     }
@@ -23,7 +23,7 @@ public class Puzzles {
     public static void main(String[] args) throws InterruptedException, IOException {
         while (true) {
             clearTerminal();
-            System.out.println("Welcome to the Universal Communication Device. Enter a code (\"stop\" to exit): ");
+            System.out.println("Welcome to the Universal Communication Device. Enter a transfer code (\"stop\" to exit): ");
             String input = System.console().readLine();
             
             System.out.println("IMPORTANT: You will only see the clue for 1 second, so be ready.");
