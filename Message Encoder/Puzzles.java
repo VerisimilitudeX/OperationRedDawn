4a884fd26b1730fc08699fdc4717cc154c29cac2
IcebergSnow <116831379+IcebergSnow@users.noreply.github.com>
2023-05-30 22:04:33 +0000

    a

--- Message Encoder/Puzzles.java
+++ Message Encoder/Puzzles.java
@@ -22,8 +22,9 @@ public class Puzzles {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         while (true) {
+            
             clearTerminal();
-            System.out.println("Welcome to the Universal Communication Device. Enter a transfer code (\"stop\" to exit): ");
+            System.out.println("Welcome to the Universal Communication Device. Enter a recipient code (\"stop\" to exit): ");
             String input = System.console().readLine();
             
             System.out.println("IMPORTANT: You will only see the clue for 1 second, so be ready.");
